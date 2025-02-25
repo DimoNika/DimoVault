7bab0c9214a5b2930c835b2a25e1a967ffdd6e3a
dima <1@dima.com>
2025-02-25 21:42:17 +0200

    Turned off logging for polling endpoint

--- app/run_uvicorn.py
+++ app/run_uvicorn.py
@@ -2,13 +2,13 @@ import uvicorn
 
 import logging
 
-# # Создаем фильтр логов для эндпоинта /is-scanned
-# class FilterPollingLogs(logging.Filter):
-#     def filter(self, record):
-#         return "POST /is-scanned" not in record.getMessage()
+# Создаем фильтр логов для эндпоинта /is-scanned
+class FilterPollingLogs(logging.Filter):
+    def filter(self, record):
+        return "POST /is-scanned" not in record.getMessage()
 
-# # Добавляем фильтр к Uvicorn логгеру
-# logging.getLogger("uvicorn.access").addFilter(FilterPollingLogs())
+# Добавляем фильтр к Uvicorn логгеру
+logging.getLogger("uvicorn.access").addFilter(FilterPollingLogs())
 
 
 if __name__ == "__main__":
